33c3be56cce35034edc378e6cea38964b7d61d2a
Gayathri Arumugam <gayathriarumugam229@gmail.com>
2024-01-08 14:22:01 +0530

    react-scripts build complete

---
+++ build/Grocery.add4300d.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequireaec2;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>a,set:void 0,enumerable:!0,configurable:!0});var t=e("ayMG0"),a=()=>(0,t.jsx)("h1",{children:"Huge Grocery component"})});
+//# sourceMappingURL=Grocery.add4300d.js.map
